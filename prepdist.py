ddfa256797497cac7cbdf63ef8da756a52e9ee94
Stefan Hechenberger <stefan@nortd.com>
2017-07-05 12:08:38 +0200

    image similarity training set generation

--- prepdist.py
+++ prepdist.py
@@ -5,6 +5,8 @@ import argparse
 import shutil
 import numpy as np
 import cv2
+import tensorflow as tf
+import tfimage as im
 
 
 
@@ -52,12 +54,16 @@ def pad(source, target):
 
 
 def flann(A_path, B_path, limit=0):
+    """Find most similar image.
+    For every image in A_path find the the most similar image in B_path."""
     FLANN_INDEX_LSH = 6
 
+    image_pairing = {}
+
     i = 0
-    for img_ in sorted(os.listdir(A_path)):
-        imgA_path = os.path.join(A_path, img_)
-        print "Searching for {0}".format(img_)
+    for imgA_name in sorted(os.listdir(A_path)):
+        imgA_path = os.path.join(A_path, imgA_name)
+        print "Searching for {0}".format(imgA_name)
         imgA = cv2.imread(imgA_path, 0)
         brisk = cv2.BRISK()
         kpA, desA = brisk.detectAndCompute(imgA, None)
@@ -68,11 +74,11 @@ def flann(A_path, B_path, limit=0):
         search_params = dict(checks=50)   # or pass empty dictionary
         flann = cv2.FlannBasedMatcher(index_params, search_params)
         imgB_names = []
-        for img__ in sorted(os.listdir(B_path)):
-            imgB_names.append(img__)
-            imgB_path = os.path.join(B_path, img__)
+        for imgB_name in sorted(os.listdir(B_path)):
+            imgB_names.append(imgB_name)
+            imgB_path = os.path.join(B_path, imgB_name)
             imgB = cv2.imread(imgB_path, 0)
-            # print "Detecting and computing {0}".format(img__)
+            # print "Detecting and computing {0}".format(imgB_name)
             kpB, desB = brisk.detectAndCompute(imgB, None)
             # print "Adding..."
             flann.add([desB])
@@ -90,15 +96,57 @@ def flann(A_path, B_path, limit=0):
             for matchpart in match:
                 matchidxs.append(matchpart.imgIdx)
         topimgidx = max(matchidxs, key=matchidxs.count)
-        print topimgidx
-        print imgB_names[topimgidx]
+        # print topimgidx
+        # print imgB_names[topimgidx]
+        image_pairing[imgA_name] = imgB_names[topimgidx]
 
         i += 1
         if limit > 0 and i > limit:
             break
 
+    return image_pairing
+
+
+
+
+def combine(pairing, A_path, B_path, dst_path):
+    # clear output path
+    if os.path.exists(dst_path):
+        shutil.rmtree(dst_path)
+    os.mkdir(dst_path)
+    with tf.Session() as sess:
+        for imgA_name, imgB_name in pairing.items():
+            imgA = im.load(os.path.join(A_path, imgA_name))
+            imgB = im.load(os.path.join(B_path, imgB_name))
+
+            # make sure that dimensions are correct
+            height, width, _ = imgA.shape
+            if height != imgB.shape[0] or width != imgB.shape[1]:
+                raise Exception("differing sizes")
+
+            # convert both images to RGB if necessary
+            if imgA.shape[2] == 1:
+                imgA = im.grayscale_to_rgb(images=imgA)
+
+            if imgB.shape[2] == 1:
+                imgB = im.grayscale_to_rgb(images=imgB)
+
+            # remove alpha channel
+            if imgA.shape[2] == 4:
+                imgA = imgA[:,:,:3]
+
+            if imgB.shape[2] == 4:
+                imgB = imgB[:,:,:3]
+
+            imgC = np.concatenate([imgA, imgB], axis=1)
+            im.save(imgC, os.path.join(dst_path,imgA_name))
+
+
+
 
 if args.cmd == 'prepraw':
     pad(rawA_path, A_path)
     pad(rawB_path, B_path)
-    flann(A_path, B_path, 4)
+    pairs = flann(A_path, B_path, 0)
+    # print pairs
+    combine(pairs, A_path, B_path, train_path)
